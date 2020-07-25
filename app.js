eb3aa62838759f87311d1a30cc13f98006047590
henry lay <henry88lay@gmail.com>
2020-07-25 10:16:12 +1000

    ran intial test with the public method 'public test' controllers cleared and clear the methods in the controller

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
+// Budget Controller
 const budgetController = (function() {
-  let x = 23;
-  const add = function(a) {
-    return x + a;
-  }
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  }
+
 })();
 
+// UI Controller
 const UIController = (function() {
 
 })();
 
+// Global App Controller
 const controller = (function(budgetCtrl, UICtrl) {
-  const z = budgetCtrl.publicTest(5);
-
-  return {
-    anotherPublic: function() {
-      console.log(z);
-    }
-  }
+  document.querySelector('.add__btn').addEventListener('click', function() {
+    // 1. Get field input data
+    // 2. Add the item to the budget controller
+    // 3. Add the item to the UI
+    // 4. Calculate the budget
+    // 5. Display the budget on the UI
+  });
+  document.addEventListener('keypress', function(event) {
+    console.log(event);
+  })
 })(budgetController, UIController);
