499f67c3471d7826cf255a56b584948f5216de9c
henry lay <henry88lay@gmail.com>
2020-07-25 12:54:01 +1000

    setting on the first event listener being the keypress and add button class

--- app.js
+++ app.js
@@ -10,14 +10,21 @@ const UIController = (function() {
 
 // Global App Controller
 const controller = (function(budgetCtrl, UICtrl) {
-  document.querySelector('.add__btn').addEventListener('click', function() {
+
+  const crtlAddItem = function() {
     // 1. Get field input data
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
-  });
+    console.log('this works')
+  }
+
+  document.querySelector('.add__btn').addEventListener('click', crtlAddItem);
+
   document.addEventListener('keypress', function(event) {
-    console.log(event);
-  })
+    if (event.keyCode === 13 || event.which === 13) {
+      crtlAddItem();
+    }
+  });
 })(budgetController, UIController);
