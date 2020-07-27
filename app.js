dd0d5763c40dbdf63397368ab65588a51b3650e8
henry lay <henry88lay@gmail.com>
2020-07-27 14:35:43 +1000

    commit the latest changes

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
 // Budget Controller
 const budgetController = (function() {
+  const Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
 
+  const Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  const data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  }
+
+  return {
+    addItem: function(type, des, val) {
+      let newItem, ID;
+      
+      if (type === 'exp') {
+        newItem new Expense(ID, des, val);
+      } else if  (type === 'inc')
+    }
+  }
 })();
 
 // UI Controller
